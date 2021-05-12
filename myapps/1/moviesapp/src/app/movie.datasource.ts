d2ac427c7c57106f4fd0406f62a6e0693e909552
ERKAN GUZEL <ErkanGuzel@enterdijital-2.local>
2021-05-12 21:22:11 +0300

    test

---
+++ myapps/1/moviesapp/src/app/movie.datasource.ts
@@ -0,0 +1,11 @@
+import {Movie} from './movie';
+
+export const Movies: Movie[] = [
+    {id: 1, name: 'shazam', description:'muazzam film',imageUrl:'1.jpg'},
+    {id: 2, name: 'amazin grace', description:'muazzam film',imageUrl:'2.jpg'},
+    {id: 3, name: 'high life', description:'muazzam film',imageUrl:'3.jpg'},
+    {id: 4, name: 'billboard', description:'muazzam film',imageUrl:'4.jpg'},
+    {id: 5, name: 'stormboy', description:'muazzam film',imageUrl:'5.jpg'},
+    {id: 6, name: 'name 6', description:'muazzam film',imageUrl:'6.jpg'},
+    {id: 7, name: 'name 7', description:'muazzam film',imageUrl:'7.jpg'},
+]
